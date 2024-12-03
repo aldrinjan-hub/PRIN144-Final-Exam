12f0d237b6e825132044f1b2384395c7f750854c
Aldrin Quitalig <aldrin_quitalig@yahoo.com>
2024-12-03 11:32:54 +0800

    editted some codes

--- api/index.js
+++ api/index.js
@@ -15,75 +15,93 @@ app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 })
 
-const songs = [{ id: 1, title: 'Treasure', genre: 'pop', releaseYear: '2024', artist:'Bruno Mars', likes: '999' }, {  id: 2, title: 'Daylight', genre: 'rap', releaseYear: '2023', artist:'Maroon 5', likes: '998'}];
-let songId = songs.length;
+// const songs = [{ id: 1, title: 'Treasure', genre: 'pop', releaseYear: '2024', artist:'Bruno Mars', likes: '999' }, {  id: 2, title: 'Daylight', genre: 'rap', releaseYear: '2023', artist:'Maroon 5', likes: '998'}];
+// let songId = songs.length;
 
 // http://localhost:4000/songs
-app.get('/songs', (req, res) => {
-    if (req.query) {
-        if (req.query.id) {
+app.get('/songs', async (req, res) => 
+{
+    if (req.query) 
+    {
+        if (req.query.id) 
+        {
             // http://localhost:4000/tasks?id=1
-            const song = songs.find((song) => song.id === parseInt(req.query.id));
-            if (song) {
-                res.json(song);
-            } else {
+            const song = await sql`SELECT FROM songs WHERE id= ${req.query.id};`;
+            if (song.rowCount> 0) 
+            {
+                res.json(task.rows[0]);
+            } 
+            else 
+            {
                 res.status(404).json();
             }
             return;
         }
     }
-
-    res.json(songs);
+const songs= await sql;`SELECT*FROM songs ORDER BY id`
+res.json(songs.rows);
 });
 
 // http://localhost:4000/songs/1
-app.get('/songs/:id', (req, res) => {
+app.get('/songs/:id', async (req, res) => 
+{
     const id = req.params.id;
-    const song = songs.find((song) => song.id === parseInt(id));
+    const song = await sql `SELECT*FROM songs WHERE id= ${id};`;
 
-    if (song) {
-        res.json(song);
-    } else {
+    if (task.rowCount>0) 
+    {
+        res.json(task.rows[0]);
+    } else 
+    {
         res.status(404).json();
     }
 });
 
 // http://localhost:4000/create - { "song": "New Song" }
-app.post('/create', (req, res) => {
-    songId++;
-    req.body.id =songId;
-    songs.push(req.body);
-    res.status(201).json();
+app.post('/create', async (req, res) => {
+    try {
+        await sql`
+            INSERT INTO songs (title, genre, releaseYear, artist, likes) 
+            VALUES (${req.body.title}, ${req.body.genre}, ${req.body.releaseYear}, ${req.body.artist}, ${req.body.likes});
+        `;
+        res.status(201).json({ message: 'Song created successfully' });
+    } catch (error) {
+        console.error('Error inserting data:', error);
+        res.status(500).json({ error: 'An error occurred while creating the song' });
+    }
 });
 
-app.put('/songs/:id', (req, res) => {
+
+app.put('/songs/:id', async (req, res) => {
     const id = req.params.id;
-    const song = songs.find((song) => song.id === parseInt(id));
+    const songUpdate = await sql`UPDATE songs SET 
+    title =${(req.body.title !=undefined? req.body.title:song.title)}, genre =${(req.body.genre !=undefined? req.body.genre:song.genre)}, releaseYear =${(req.body.releaseYear !=undefined? req.body.title:song.releaseYear)}, 
+    artist =${(req.body.artist !=undefined? req.body.artist:song.artist)}, 
+    likes =${(req.body.likes !=undefined? req.body.title:song.likes)}`
 
-    if (song) {
-        song.id = parseInt(id);
-        song.title = (req.body.title != undefined ? req.body.title : song.title);
-        song.genre = (req.body.genre != undefined ? req.body.genre : song.genre);
-        song.releaseYear = (req.body.releaseYear != undefined ? req.body.releaseYear : song.releaseYear);
-        song.artist = (req.body.artist != undefined ? req.body.artist : song.artist);
-        song.likes = (req.body.likes != undefined ? req.body.likes : song.likes);
-
+    if (songUpdate.rowCount>0) 
+    {
         res.json(song);
-    } else {
+    } 
+    else 
+    {
         res.status(404).json();
     }
 });
 
 
 // http://localhost:4000/tasks/1
-app.delete('/songs/:id', (req, res) => {
+app.delete('/songs/:id', async (req, res) => {
     const id = req.params.id;
-    const song = songs.find((song) => song.id === parseInt(id));
-
-    if (song) {
-        songs.splice(songs.indexOf(song), 1);
+    const song = await sql`DELETE FROM songs WHERE id = ${id};`;
+    if (song.rowCount > 0) 
+    {
         res.status(204).json();
-    } else {
+    } 
+    else 
+    {
         res.status(404).json();
     }
 });
+
+module.exports= app;
